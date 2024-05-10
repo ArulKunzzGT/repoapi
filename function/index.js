1769b157afb63c76769fbe23ecc820df270042f4
Belial <100026757+ArulKunzzGT@users.noreply.github.com>
2024-05-10 07:53:03 +0700

    Update index.js tiktokdl

--- function/index.js
+++ function/index.js
@@ -1,7 +1,9 @@
+const tiktokdl = require('./scraper/tiktokdl');
+
 module.exports = {
 	blackboxAIChat: require('./scraper/blackboxAIChat'),
 	smartContract: require('./scraper/smartContract'),
 	ragBot: require('./scraper/ragBot'),
-	degreeGuru: require('./scraper/degreeGuru')
- tiktokdl: require('./scraper/tiktokdl')
-}
+	degreeGuru: require('./scraper/degreeGuru'),
+	tiktokdl: tiktokdl
+};
