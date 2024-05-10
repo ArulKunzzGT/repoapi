f97b13ac1ee7bcd1ce7d9476a317b5dc953b9ece
Belial <100026757+ArulKunzzGT@users.noreply.github.com>
2024-05-10 07:49:13 +0700

    Update tiktokdl.js

--- function/scraper/tiktokdl.js
+++ function/scraper/tiktokdl.js
@@ -1,13 +1,10 @@
-
 const axios = require('axios');
 
 async function tiktokdl(url) {
   try {
     const response = await axios.get(`https://skizo.tech/api/tiktok?apikey=Alok&url=${url}`);
-
     const tiktokData = response.data;
     return tiktokData;
-    
   } catch (error) {
     return { error: 'Terjadi kesalahan dalam memproses data TikTok' };
   }
